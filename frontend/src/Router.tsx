446717bb8ae546fb54802d2a897dc24798b4bebd
sipan2504 <sipan2504@hotmail.com>
2026-09-09 11:36:35 +0300

    Add PioneerMapPage to router configuration

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Shop from "./pages/Shop";
 import EngagementTasksPage from "./pages/EngagementTasksPage.tsx";
+import PioneerMapPage from "./pages/PioneerMapPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    element: <PioneerMapPage />,
+  },
+  {
+    path: "/shop",
     element: <Shop />,
   },
   {
