cb8983fe11b363e126666ac6cef02c3fce7b9788
sipan2504 <sipan2504@hotmail.com>
2026-09-09 22:40:03 +0300

    Refactor MongoDB connection and endpoint mounting

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,17 +8,21 @@ import logger from "morgan";
 import MongoStore from "connect-mongo";
 import { MongoClient } from "mongodb";
 import env from "./environments";
+
 import mountPaymentsEndpoints from "./handlers/payments";
 import mountUserEndpoints from "./handlers/users";
-
-// We must import typedefs for ts-node-dev to pick them up when they change (even though tsc would supposedly
-// have no problem here)
-// https://stackoverflow.com/questions/65108033/property-user-does-not-exist-on-type-session-partialsessiondata#comment125163548_65381085
-import "./types/session";
 import mountNotificationEndpoints from "./handlers/notifications";
+import mountPlaceEndpoints from "./handlers/places";
+
+import "./types/session";
 
 const dbName = env.mongo_db_name;
-const mongoUri = `mongodb+srv://${encodeURIComponent(env.mongo_user)}:${encodeURIComponent(env.mongo_password)}@${env.mongo_host}/${dbName}?retryWrites=true&w=majority`;
+
+const mongoUri =
+  `mongodb+srv://${encodeURIComponent(env.mongo_user)}:` +
+  `${encodeURIComponent(env.mongo_password)}@` +
+  `${env.mongo_host}/${dbName}?retryWrites=true&w=majority`;
+
 const mongoClientOptions = {
   authSource: "admin",
   auth: {
@@ -27,26 +31,26 @@ const mongoClientOptions = {
   },
 };
 
-//
-// I. Initialize and set up the express app and various middlewares and packages:
-//
-
 const app: express.Application = express();
 
-// Log requests to the console in a compact format:
 app.use(logger("dev"));
 
-// Full log of all requests to /log/access.log:
 app.use(
   logger("common", {
-    stream: fs.createWriteStream(path.join(__dirname, "..", "log", "access.log"), { flags: "a" }),
+    stream: fs.createWriteStream(
+      path.join(
+        __dirname,
+        "..",
+        "log",
+        "access.log",
+      ),
+      { flags: "a" },
+    ),
   }),
 );
 
-// Enable response bodies to be sent as JSON:
 app.use(express.json());
 
-// Handle CORS:
 app.use(
   cors({
     origin: env.frontend_url,
@@ -54,10 +58,8 @@ app.use(
   }),
 );
 
-// Handle cookies 🍪
 app.use(cookieParser());
 
-// Use sessions:
 app.set("trust proxy", 1);
 
 app.use(
@@ -79,46 +81,75 @@ app.use(
   }) as unknown as express.RequestHandler,
 );
 
-//
-// II. Mount app endpoints:
-//
-
-// Payments endpoint under /payments:
+// Payments
 const paymentsRouter = express.Router();
 mountPaymentsEndpoints(paymentsRouter);
 app.use("/payments", paymentsRouter);
 
-// User endpoints (e.g signin, signout) under /user:
+// Users
 const userRouter = express.Router();
 mountUserEndpoints(userRouter);
 app.use("/user", userRouter);
 
-// Notification endpoints under /notifications:
+// Notifications
 const notificationRouter = express.Router();
-mountNotificationEndpoints(notificationRouter);
-app.use("/notifications", notificationRouter);
+mountNotificationEndpoints(
+  notificationRouter,
+);
+app.use(
+  "/notifications",
+  notificationRouter,
+);
 
-// Hello World page to check everything works:
+// Places
+const placesRouter = express.Router();
+mountPlaceEndpoints(placesRouter);
+app.use("/api", placesRouter);
+
+// Hello World
 app.get("/", async (_, res) => {
-  res.status(200).send({ message: "Hello, World!" });
+  res.status(200).send({
+    message: "Hello, World!",
+  });
 });
-
-// III. Boot up the app:
 
 const start = async () => {
   try {
-    const client = await MongoClient.connect(mongoUri, mongoClientOptions);
+    const client = await MongoClient.connect(
+      mongoUri,
+      mongoClientOptions,
+    );
+
     const db = client.db(dbName);
-    app.locals.orderCollection = db.collection("orders");
-    app.locals.userCollection = db.collection("users");
-    console.log("Connected to MongoDB on: ", mongoUri);
+
+    app.locals.orderCollection =
+      db.collection("orders");
+
+    app.locals.userCollection =
+      db.collection("users");
+
+    app.locals.placeCollection =
+      db.collection("places");
+
+    console.log(
+      "Connected to MongoDB",
+    );
 
     app.listen(env.port, () => {
-      console.log(`App platform demo app - Backend listening on port ${env.port}!`);
-      console.log(`CORS config: configured to respond to a frontend hosted on ${env.frontend_url}`);
+      console.log(
+        `PioneerMap Backend listening on port ${env.port}!`,
+      );
+
+      console.log(
+        `CORS configured for ${env.frontend_url}`,
+      );
     });
   } catch (err) {
-    console.error("Connection to MongoDB failed: ", err);
+    console.error(
+      "Connection to MongoDB failed:",
+      err,
+    );
+
     process.exit(1);
   }
 };
